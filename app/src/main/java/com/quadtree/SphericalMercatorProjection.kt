23514ce1013ff3e0e7431a390748e1831c2621a8
Mark(mazhenjin) <mazhenjin@sina.com>
2021-11-21 15:31:34 +0800

    import NonHierarchicalDistanceBasedAlgorithmTest.kt;append lost file

---
+++ app/src/main/java/com/quadtree/SphericalMercatorProjection.kt
@@ -0,0 +1,20 @@
+package com.quadtree
+
+import com.amap.api.maps.model.LatLng
+
+class SphericalMercatorProjection(val mWorldWidth: Double) {
+    fun toPoint(latLng: LatLng): Point {
+        val x = latLng.longitude / 360 + .5
+        val siny = Math.sin(Math.toRadians(latLng.latitude))
+        val y = 0.5 * Math.log((1 + siny) / (1 - siny)) / -(2 * Math.PI) + .5
+        return Point(x * mWorldWidth, y * mWorldWidth)
+    }
+
+    fun toLatLng(point: Point): LatLng {
+        val x = point.x / mWorldWidth - 0.5
+        val lng = x * 360
+        val y = .5 - point.y / mWorldWidth
+        val lat = 90 - Math.toDegrees(Math.atan(Math.exp(-y * 2 * Math.PI)) * 2)
+        return LatLng(lat, lng)
+    }
+}
