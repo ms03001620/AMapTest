17df6a59c46788ec136d1c7d3051ef349d9b591a
Mark(mazhenjin) <mazhenjin@sina.com>
2021-11-21 16:26:39 +0800

    convert java to kt

--- app/src/main/java/com/quadtree/NonHierarchicalDistanceBasedAlgorithm.kt
+++ app/src/main/java/com/quadtree/NonHierarchicalDistanceBasedAlgorithm.kt
@@ -1,28 +1,22 @@
-package com.quadtree;
+package com.quadtree
 
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.HashMap
+import java.util.HashSet
+import java.util.LinkedHashSet
 
-public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> {
-    private static final int DEFAULT_MAX_DISTANCE_AT_ZOOM = 100; // essentially 100 dp.
-
-    private int mMaxDistance = DEFAULT_MAX_DISTANCE_AT_ZOOM;
+class NonHierarchicalDistanceBasedAlgorithm<T : ClusterItem> {
+    private val maxDistance = DEFAULT_MAX_DISTANCE_AT_ZOOM
 
     /**
      * Any modifications should be synchronized on mQuadTree.
      */
-    private final Collection<QuadItem<T>> mQuadList = new LinkedHashSet<>();
+    private val quadList: MutableCollection<QuadItem<T>> = LinkedHashSet()
 
     /**
      * Any modifications should be synchronized on mQuadTree.
      */
-    private final PointQuadTree<QuadItem<T>> mQuadTree = new PointQuadTree<QuadItem<T>>(
-            new Bounds(.0, 1.0, .0, 1.0), 0);
+    private val quadTree = PointQuadTree<QuadItem<T>>(Bounds(.0, 1.0, .0, 1.0))
 
     /**
      * Adds an item to the algorithm
@@ -30,16 +24,16 @@ public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> {
      * @param item the item to be added
      * @return true if the algorithm contents changed as a result of the call
      */
-    public boolean addItem(T item) {
-        boolean result;
-        final QuadItem<T> quadItem = new QuadItem<>(item);
-        synchronized (mQuadTree) {
-            result = mQuadList.add(quadItem);
+    fun addItem(item: T): Boolean {
+        var result: Boolean
+        val quadItem = QuadItem(item)
+        synchronized(quadTree) {
+            result = quadList.add(quadItem)
             if (result) {
-                mQuadTree.add(quadItem);
+                quadTree.add(quadItem)
             }
         }
-        return result;
+        return result
     }
 
     /**
@@ -48,21 +42,21 @@ public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> {
      * @param items the items to be added
      * @return true if the algorithm contents changed as a result of the call
      */
-    public boolean addItems(Collection<T> items) {
-        boolean result = false;
-        for (T item : items) {
-            boolean individualResult = addItem(item);
+    fun addItems(items: Collection<T>): Boolean {
+        var result = false
+        for (item in items) {
+            val individualResult = addItem(item)
             if (individualResult) {
-                result = true;
+                result = true
             }
         }
-        return result;
+        return result
     }
 
-    public void clearItems() {
-        synchronized (mQuadTree) {
-            mQuadList.clear();
-            mQuadTree.clear();
+    fun clearItems() {
+        synchronized(quadTree) {
+            quadList.clear()
+            quadTree.clear()
         }
     }
 
@@ -73,18 +67,18 @@ public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> {
      * @return true if this algorithm contained the specified element (or equivalently, if this
      * algorithm changed as a result of the call).
      */
-    public boolean removeItem(T item) {
-        boolean result;
+    fun removeItem(item: T): Boolean {
+        var result: Boolean
         // QuadItem delegates hashcode() and equals() to its item so,
         //   removing any QuadItem to that item will remove the item
-        final QuadItem<T> quadItem = new QuadItem<>(item);
-        synchronized (mQuadTree) {
-            result = mQuadList.remove(quadItem);
+        val quadItem = QuadItem(item)
+        synchronized(quadTree) {
+            result = quadList.remove(quadItem)
             if (result) {
-                mQuadTree.remove(quadItem);
+                quadTree.remove(quadItem)
             }
         }
-        return result;
+        return result
     }
 
     /**
@@ -93,21 +87,21 @@ public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> {
      * @param items the items to be removed
      * @return true if this algorithm contents changed as a result of the call
      */
-    public boolean removeItems(Collection<T> items) {
-        boolean result = false;
-        synchronized (mQuadTree) {
-            for (T item : items) {
+    fun removeItems(items: Collection<T>): Boolean {
+        var result = false
+        synchronized(quadTree) {
+            for (item in items) {
                 // QuadItem delegates hashcode() and equals() to its item so,
                 //   removing any QuadItem to that item will remove the item
-                final QuadItem<T> quadItem = new QuadItem<>(item);
-                boolean individualResult = mQuadList.remove(quadItem);
+                val quadItem = QuadItem(item)
+                val individualResult = quadList.remove(quadItem)
                 if (individualResult) {
-                    mQuadTree.remove(quadItem);
-                    result = true;
+                    quadTree.remove(quadItem)
+                    result = true
                 }
             }
         }
-        return result;
+        return result
     }
 
     /**
@@ -117,93 +111,90 @@ public class NonHierarchicalDistanceBasedAlgorithm<T extends ClusterItem> {
      * @return true if the item existed in the algorithm and was updated, or false if the item did
      * not exist in the algorithm and the algorithm contents remain unchanged.
      */
-    public boolean updateItem(T item) {
+    fun updateItem(item: T): Boolean {
         // TODO - Can this be optimized to update the item in-place if the location hasn't changed?
-        boolean result;
-        synchronized (mQuadTree) {
-            result = removeItem(item);
+        var result: Boolean
+        synchronized(quadTree) {
+            result = removeItem(item)
             if (result) {
                 // Only add the item if it was removed (to help prevent accidental duplicates on map)
-                result = addItem(item);
+                result = addItem(item)
             }
         }
-        return result;
+        return result
     }
 
-    public Set<? extends Cluster<T>> getClusters(float zoom) {
-        final int discreteZoom = (int) zoom;
-
-        final double zoomSpecificSpan = mMaxDistance / Math.pow(2, discreteZoom) / 256;
-
-        final Set<QuadItem<T>> visitedCandidates = new HashSet<>();
-        final HashSet<Cluster<T>> results = new HashSet<>();
-        final Map<QuadItem<T>, Double> distanceToCluster = new HashMap<>();
-        final Map<QuadItem<T>, StaticCluster<T>> itemToCluster = new HashMap<>();
-
-        synchronized (mQuadTree) {
-            for (QuadItem<T> candidate : mQuadList) {
+    fun getClusters(zoom: Float): Set<Cluster<T>> {
+        val discreteZoom = zoom.toInt()
+        val zoomSpecificSpan = maxDistance / Math.pow(2.0, discreteZoom.toDouble()) / 256
+        val visitedCandidates: MutableSet<QuadItem<T>> = HashSet()
+        val results = HashSet<Cluster<T>>()
+        val distanceToCluster: MutableMap<QuadItem<T>, Double> = HashMap()
+        val itemToCluster: MutableMap<QuadItem<T>, StaticCluster<T>> = HashMap()
+        synchronized(quadTree) {
+            for (candidate in quadList) {
                 if (visitedCandidates.contains(candidate)) {
                     // Candidate is already part of another cluster.
-                    continue;
+                    continue
                 }
-
-                Bounds searchBounds = createBoundsFromSpan(candidate.getPoint(), zoomSpecificSpan);
-                Collection<QuadItem<T>> searchBoundsItems = mQuadTree.search(searchBounds);
-                if (searchBoundsItems.size() == 1) {
+                val searchBounds = createBoundsFromSpan(candidate.point, zoomSpecificSpan)
+                val searchBoundsItems = quadTree.search(searchBounds)
+                if (searchBoundsItems.size == 1) {
                     // Only the current marker is in range. Just add the single item to the results.
-                    results.add(candidate);
-                    visitedCandidates.add(candidate);
-                    distanceToCluster.put(candidate, 0d);
-                    continue;
+                    results.add(candidate)
+                    visitedCandidates.add(candidate)
+                    distanceToCluster[candidate] = 0.0
+                    continue
                 }
-                StaticCluster staticCluster = new StaticCluster(candidate.getClusterItem().getPosition());
-                results.add(staticCluster);
-
-                for (QuadItem<T> clusterItem : searchBoundsItems) {
-                    double distance = distanceSquared(clusterItem.getPoint(), candidate.getPoint());
-
-                    Double existingDistance = distanceToCluster.get(clusterItem);
+                val staticCluster = StaticCluster<T>(candidate.clusterItem.position)
+                results.add(staticCluster)
+                for (clusterItem in searchBoundsItems) {
+                    val distance = distanceSquared(clusterItem.point, candidate.point)
+                    val existingDistance = distanceToCluster[clusterItem]
                     if (existingDistance != null) {
                         // Item already belongs to another cluster. Check if it's closer to this cluster.
                         if (existingDistance < distance) {
-                            continue;
+                            continue
                         }
                         // Move item to the closer cluster.
-                        itemToCluster.get(clusterItem).remove(clusterItem.getClusterItem());
+                        itemToCluster[clusterItem]!!.remove(clusterItem.clusterItem)
                     }
-                    distanceToCluster.put(clusterItem, distance);
-                    staticCluster.add(clusterItem.getClusterItem());
-                    itemToCluster.put(clusterItem, staticCluster);
+                    distanceToCluster[clusterItem] = distance
+                    staticCluster.add(clusterItem.clusterItem)
+                    itemToCluster[clusterItem] = staticCluster
                 }
-                visitedCandidates.addAll(searchBoundsItems);
+                visitedCandidates.addAll(searchBoundsItems)
             }
         }
-        return results;
+        return results
     }
 
-
-    public Collection<T> getItems() {
-        final Set<T> items = new LinkedHashSet<>();
-        synchronized (mQuadTree) {
-            for (QuadItem<T> quadItem : mQuadList) {
-                items.add(quadItem.getClusterItem());
+    val items: Collection<T>
+        get() {
+            val items: MutableSet<T> = LinkedHashSet()
+            synchronized(quadTree) {
+                for (quadItem in quadList) {
+                    items.add(quadItem.clusterItem)
+                }
             }
+            return items
         }
-        return items;
+
+    private fun distanceSquared(a: Point, b: Point): Double {
+        return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)
     }
 
-    private double distanceSquared(Point a, Point b) {
-        return (a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY());
-    }
-
-    private Bounds createBoundsFromSpan(Point p, double span) {
+    private fun createBoundsFromSpan(p: Point, span: Double): Bounds {
         // TODO: Use a span that takes into account the visual size of the marker, not just its
         // LatLng.
-        double halfSpan = span / 2;
-        return new Bounds(
-                p.getX() - halfSpan, p.getX() + halfSpan,
-                p.getY() - halfSpan, p.getY() + halfSpan);
+        val halfSpan = span / 2
+        return Bounds(
+            p.x - halfSpan, p.x + halfSpan,
+            p.y - halfSpan, p.y + halfSpan
+        )
     }
 
-
+    companion object {
+        private const val DEFAULT_MAX_DISTANCE_AT_ZOOM = 100 // essentially 100 dp.
+    }
 }
