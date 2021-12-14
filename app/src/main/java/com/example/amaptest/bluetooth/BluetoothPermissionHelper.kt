9910f1bf5be2cd568d31703b8e0858bb68238508
Mark(mazhenjin) <mazhenjin@sina.com>
2021-12-14 18:04:32 +0800

    modify deny permission setting page

--- app/src/main/java/com/example/amaptest/bluetooth/BluetoothPermissionHelper.kt
+++ app/src/main/java/com/example/amaptest/bluetooth/BluetoothPermissionHelper.kt
@@ -6,37 +6,84 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.amaptest.CommonAskDialog
 import com.example.amaptest.LocationUtils
+import com.example.amaptest.R
 
 class BluetoothPermissionHelper(
     private val activity: AppCompatActivity
 ) {
     private var callback: (() -> Unit)? = null
 
-    private var requestBluetoothLePermissionLauncher =
+    private var requestMultiplePermissionLauncher =
         activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { allGrants ->
             if (allGrants.values.all { it }) {
                 attemptGotoBluetoothLePage()
             } else {
-                Toast.makeText(activity, "已取消蓝牙授权", Toast.LENGTH_LONG).show()
+                showAlertDialog(
+                    activity,
+                    R.string.permission_prompt_location,
+                    leftCallback = {
+                        Toast.makeText(activity, "已取消定位授权", Toast.LENGTH_LONG).show()
+                    },
+                    rightCallback = {
+                        try {
+                            activity.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                        } catch (e: Exception) {
+                            activity.startActivity(Intent(Settings.ACTION_SETTINGS))
+                        }
+                    })
             }
         }
 
-    private var requestBLEOnlyFinePermissionLauncher =
+    private var requestMultipleBPermissionLauncher =
         activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { allGrants ->
             if (allGrants.values.all { it }) {
                 attemptGotoBluetoothLePage()
             } else {
-                LocationUtils.goSettingForBluetooth(activity) {
-                    Toast.makeText(activity, "已取消位置授权", Toast.LENGTH_LONG).show()
-                }
+                showAlertDialog(
+                    activity,
+                    R.string.permission_prompt_bluetooth,
+                    leftCallback = {
+                        Toast.makeText(activity, "已取消蓝牙授权", Toast.LENGTH_LONG).show()
+                    },
+                    rightCallback = {
+                        try {
+                            Intent(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                Uri.fromParts("package", activity.packageName, null)
+                            ).let {
+                                activity.startActivity(it)
+                            }
+                        } catch (e: Exception) {
+                            //ignore
+                        }
+                    })
             }
         }
+
+    private fun showAlertDialog(
+        context: Context,
+        messageResId: Int,
+        leftCallback: (() -> Unit)? = null,
+        rightCallback: (() -> Unit)? = null
+    ) {
+        CommonAskDialog.Builder(
+            context,
+            context.getString(R.string.charging_open_settings),
+            context.getString(R.string.cancel_option),
+            leftCallback = leftCallback
+        ).create(
+            context.getString(messageResId), listener = { rightCallback?.invoke() })
+    }
+
 
     private var requestBluetooth =
         activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -99,7 +146,7 @@ class BluetoothPermissionHelper(
         ) {
             callback.invoke()
         } else {
-            requestBLEOnlyFinePermissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
+            requestMultiplePermissionLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
         }
     }
 
@@ -117,6 +164,7 @@ class BluetoothPermissionHelper(
 
     private fun checkBluetoothPermission(callback: () -> Unit) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // setup targetSdk 31; show system dialog
             if (ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_SCAN)
                 == PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_CONNECT)
@@ -124,10 +172,10 @@ class BluetoothPermissionHelper(
             ) {
                 callback.invoke()
             } else {
-                requestBluetoothLePermissionLauncher.launch(
+                requestMultipleBPermissionLauncher.launch(
                     arrayOf(
                         Manifest.permission.BLUETOOTH_CONNECT,
-                        Manifest.permission.BLUETOOTH_SCAN
+                        Manifest.permission.BLUETOOTH_SCAN,
                     )
                 )
             }
