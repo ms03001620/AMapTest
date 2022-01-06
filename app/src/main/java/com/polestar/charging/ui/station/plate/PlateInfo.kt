16a70575e58f878c7e776a16cbf61bcffcaac139
Mark(mazhenjin) <mazhenjin@sina.com>
2022-01-06 10:17:49 +0800

    refactor

--- app/src/main/java/com/polestar/charging/ui/station/plate/PlateInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.polestar.charging.ui.station.plate
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class PlateInfo(val defaultVin: String?, val plates: List<Plate>): Parcelable
