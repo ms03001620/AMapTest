d796523e41e94067cf972b6325e11ab917a15df4
Mark(mazhenjin) <mazhenjin@sina.com>
2022-02-18 17:07:19 +0800

    add robolectric test stub

--- app/src/main/java/com/robolectric/WelcomeActivity.kt
+++ app/src/main/java/com/robolectric/WelcomeActivity.kt
@@ -1,20 +1,43 @@
 package com.robolectric
 
-import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.amaptest.R
-
+import com.example.amaptest.marker.IconGenerator
+import com.polestar.base.utils.logd
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        val iconCluster =
+            IconGenerator(
+                this,
+                R.layout.charging_layout_marker_cluster_v3,
+                Color.BLACK,
+                offsetHeight4Text = 0
+            )
+
+        iconCluster.makeIcon("init")
+
         val button = findViewById<View>(R.id.login)
         button.setOnClickListener {
-            startActivity(Intent(this@WelcomeActivity, LoginActivity::class.java))
+            //startActivity(Intent(this@WelcomeActivity, LoginActivity::class.java))
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val start = System.currentTimeMillis()
+                for(i in 0..100){
+                    iconCluster.makeIcon(i.toString())
+                }
+                logd("pass:${System.currentTimeMillis()-start}", "IconGenerator")
+            }
+
         }
     }
 }
