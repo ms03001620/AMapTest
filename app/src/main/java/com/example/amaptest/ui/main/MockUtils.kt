482163652135d0dc3fa097dc61bd59bfd354d26a
ZhenjinMa <mazhenjin@sina.com>
2022-07-10 10:36:32 +0800

    add position

--- app/src/main/java/com/example/amaptest/ui/main/MockUtils.kt
+++ app/src/main/java/com/example/amaptest/ui/main/MockUtils.kt
@@ -4,9 +4,15 @@ import com.amap.api.maps.model.LatLng
 
 object MockUtils {
 
+    //白玉兰
     fun mockBaiYulan() = LatLng(DEFAULT_LAT, DEFAULT_LNG)
+
+
+    //上海黄浦区领展企业广场
+    fun mockLingZhan() = LatLng(31.22128, 121.476231)
 
     // 白玉兰
     const val DEFAULT_LNG = 121.497798
     const val DEFAULT_LAT = 31.249051
+
 }
