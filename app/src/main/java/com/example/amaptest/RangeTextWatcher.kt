6dbc1ca12b38526cf075854911c85f961128d734
ZhenjinMa <mazhenjin@sina.com>
2024-01-30 13:29:35 +0800

    add RangeTextWatcher.kt

---
+++ app/src/main/java/com/example/amaptest/RangeTextWatcher.kt
@@ -0,0 +1,52 @@
+package com.example.amaptest
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Range
+
+/**
+ * 判断用户输入值是否在指定范围内
+ */
+class RangeTextWatcher(
+    private val range: Range<Int>,
+    private val callback: OnWatcherCallback
+) : TextWatcher {
+
+    private var uncheckString = ""
+    private val handler = Handler(Looper.getMainLooper(), object : Handler.Callback {
+        override fun handleMessage(msg: Message): Boolean {
+            return false
+        }
+    })
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    override fun afterTextChanged(s: Editable) {
+        uncheckString = s.toString()
+        handler.removeCallbacks(checkRunnable)
+        handler.postDelayed(checkRunnable, 1000)
+    }
+
+    private val checkRunnable = Runnable {
+        val input = uncheckString.toIntOrNull()
+
+        if (input != null && range.contains(input)) {
+            callback.onInRange(input, range)
+        } else {
+            callback.onOutRange(input, range)
+        }
+    }
+
+    interface OnWatcherCallback {
+        fun onOutRange(input: Int?, range: Range<Int>)
+        fun onInRange(input: Int, range: Range<Int>)
+    }
+
+}
