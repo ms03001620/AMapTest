493f130d9f645349a59dc760d68d103b47baaa1a
ZhenjinMa <mazhenjin@sina.com>
2024-03-01 17:11:53 +0800

    save

--- app/src/main/java/com/example/amaptest/MaskFrameLayout.kt
+++ app/src/main/java/com/example/amaptest/MaskFrameLayout.kt
@@ -11,12 +11,16 @@ import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.graphics.RectF
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toBitmapOrNull
 
 @SuppressLint("AppCompatCustomView")
-class MaskFrameLayout(context: Context, attrs: AttributeSet?) :FrameLayout(context, attrs) {
+class MaskFrameLayout(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
 
     var mPaint = Paint()
@@ -24,46 +28,52 @@ class MaskFrameLayout(context: Context, attrs: AttributeSet?) :FrameLayout(conte
     var H = 100
 
 
-/*    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        canvas.drawColor(Color.GREEN)
-        val sc = canvas.saveLayer(0f, 0f, W.toFloat(), H.toFloat(), null, Canvas.ALL_SAVE_FLAG)
-        canvas.drawBitmap(makeDst(W, H), 0f, 0f, mPaint)
-        mPaint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
-        canvas.drawBitmap(makeMask(W, H), 0f, 0f, mPaint)
-        mPaint.setXfermode(null)
-        canvas.restoreToCount(sc)
-    }
-    */
-    var mFinalMask: Bitmap?= null//akeMask(100, 100)
+    /*    override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            canvas.drawColor(Color.GREEN)
+            val sc = canvas.saveLayer(0f, 0f, W.toFloat(), H.toFloat(), null, Canvas.ALL_SAVE_FLAG)
+            canvas.drawBitmap(makeDst(W, H), 0f, 0f, mPaint)
+            mPaint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
+            canvas.drawBitmap(makeMask(W, H), 0f, 0f, mPaint)
+            mPaint.setXfermode(null)
+            canvas.restoreToCount(sc)
+        }
+        */
+    var mFinalMask: Bitmap? = null//akeMask(100, 100)
 
-    var matrixMask =  android.graphics.Matrix()
+    var matrixMask = android.graphics.Matrix()
 
     override fun dispatchDraw(canvas: Canvas) {
-        background.draw(canvas)
+        background?.draw(canvas)
 
-        val sc = canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null, Canvas.ALL_SAVE_FLAG)
+        val sc =
+            canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null, Canvas.ALL_SAVE_FLAG)
 
         super.dispatchDraw(canvas)
 
         val childFirst = getChildAt(0)
-        if(mFinalMask==null && childFirst is ImageView){
-            val drawable = childFirst.drawable as BitmapDrawable
-
-            mFinalMask = drawable.getBitmap()
-            matrixMask=childFirst.imageMatrix
+        if (mFinalMask == null && childFirst is ImageView) {
+            mFinalMask = drawableToBitmap(childFirst.drawable)
+            matrixMask = childFirst.imageMatrix
             mPaint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.DST_IN))
         }
 
-        mFinalMask?.let {mFinalMask->
+        mFinalMask?.let { mFinalMask ->
             canvas.drawBitmap(mFinalMask, matrixMask, mPaint)
         }
 
         canvas.restoreToCount(sc)
-
-
     }
 
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+        if (drawable is GradientDrawable) {
+            drawable.toBitmapOrNull()
+        }
+        throw UnsupportedOperationException()
+    }
 
 
     companion object {
