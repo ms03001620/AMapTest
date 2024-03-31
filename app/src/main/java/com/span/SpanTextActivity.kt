c082b436c543b13bd9340ee4c1c35448e44e3687
ZhenjinMa <mazhenjin@sina.com>
2024-03-31 13:42:02 +0800

    add page for span; layout;sp1

--- app/src/main/java/com/span/SpanTextActivity.kt
+++ app/src/main/java/com/span/SpanTextActivity.kt
@@ -1,12 +1,30 @@
 package com.span
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.amaptest.R
+
 
 class SpanTextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_span_text)
+
+        val textSpan = findViewById<TextView>(R.id.textSpan)
+
+        val string = "2020-18-20 12:15 最早"
+
+        val spannable: Spannable = SpannableString(string)
+        spannable.setSpan(
+            TagTextSpan(),
+            string.length - 2,
+            string.length,
+            SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textSpan.text = spannable
     }
 }
---
+++ app/src/main/java/com/span/TagTextSpan.kt
@@ -0,0 +1,107 @@
+package com.span
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.text.TextPaint
+import android.text.style.ReplacementSpan
+import kotlin.math.roundToInt
+
+class TagTextSpan(
+    val bgColor: Int = Color.parseColor("#FF7500"),
+    val scale: Float = .7f,
+    val tagTextColor: Int = Color.parseColor("#FFFFFF"),
+) : ReplacementSpan() {
+    private var width = 0.0f
+    private var tagWidth = 0.0f
+    private val bgPaint = Paint()
+    private lateinit var paintScale: Paint
+    private val scaleTextRect = Rect()
+    private val bgRectF = RectF()
+
+    override fun getSize(
+        paint: Paint,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        fm: Paint.FontMetricsInt?
+    ): Int {
+        bgPaint.setColor(bgColor)
+        width = paint.measureText(text.toString(), 0, text.toString().length)
+        tagWidth = paint.measureText(text.toString(), start, end)
+
+        paintScale = TextPaint(paint)
+        paintScale.textSize = paint.textSize * scale
+        paintScale.color = tagTextColor
+        paintScale.textAlign = Paint.Align.CENTER
+        paintScale.getTextBounds(text.toString(), start, end, scaleTextRect)
+
+        return tagWidth.roundToInt()
+    }
+
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        drawBg(canvas = canvas, top = top, bottom = bottom)
+        drawOriginText(canvas, text, start, end, x, top, y, bottom, paint)
+
+        drawTagText(canvas, text, start, end, bgRectF, paintScale)
+
+
+    }
+
+    private fun drawTagText(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        bgRectF: RectF,
+        paintScale: Paint
+    ) {
+        canvas.drawText(
+            text.toString(),
+            start,
+            end,
+            bgRectF.centerX(),
+            bgRectF.centerY() + scaleTextRect.height() / 2,
+            paintScale
+        )
+    }
+
+    private fun drawOriginText(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        canvas.drawText(text.toString(), start, end, x, y.toFloat(), paint)
+    }
+
+    private fun drawBg(
+        canvas: Canvas,
+        top: Int,
+        bottom: Int,
+    ) {
+        bgRectF.left = width - tagWidth
+        bgRectF.top = top.toFloat()
+        bgRectF.right = width
+        bgRectF.bottom = bottom.toFloat()
+
+        canvas.drawRect(bgRectF, bgPaint)
+    }
+}
